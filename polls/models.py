f868f8fff018c0b0bc13e8cb2a3087675795f6f0
jhp2060 <35055301+jhp2060@users.noreply.github.com>
2019-09-29 01:41:18 +0900

    [10기_진혁팍] Django tutorial 미션 제출합니다. (#9)
    * Update README.md
    
    * part5 completed & updated readme about issues of using mysql
    
    * modified #s on readme
    
    * indent
    
    * part6_static files & part7_admin customizing & how to insert images on readme

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,18 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
     def __str__(self):
         return self.question_text
 
@@ -19,4 +25,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
